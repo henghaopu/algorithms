b8f5d81f0e02bf995d65e729fee2025398766bd7
Heng-Hao Pu <hpu@oswego.edu>
2020-02-16 00:00:29 -0500

    Implement bubble sort

---
+++ sorting/bubble-sort/app.js
@@ -0,0 +1,21 @@
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function bubbleSort(array) {
+  let upperBound = array.length - 1;
+  for (let i = 0; i < upperBound; ) {
+    for (let j = 0; j < upperBound; j++) {
+      if (array[j] > array[j + 1]) {
+        debugger;
+        // swap
+        const temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+    upperBound--;
+  }
+  return array;
+}
+
+bubbleSort(numbers);
+console.log(numbers);
