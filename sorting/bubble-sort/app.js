32505ad37aa07a843423fc111d13dfde4e751152
Heng-Hao Pu <hpu@oswego.edu>
2020-04-27 16:41:38 -0400

    Implement bubble sort

--- sorting/bubble-sort/app.js
+++ sorting/bubble-sort/app.js
@@ -1,21 +1,20 @@
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
-// like challenge passing game or LOL
-function bubbleSort(array) {
-  let upperBound = array.length - 1;
-  for (let i = 0; i < upperBound; ) {
-    for (let j = 0; j < upperBound; j++) {
-      if (array[j] > array[j + 1]) {
-        // swap
-        const temp = array[j];
-        array[j] = array[j + 1];
-        array[j + 1] = temp;
-      }
+function bubbleSort(arr) {
+  let upperBound = arr.length - 1;
+  // repeat arr.length times
+  for (let repeat = 0; repeat < arr.length; repeat++) {
+    // walk through each pair
+    for (let i = 0; i < upperBound; i++) {
+      // IF > THEN swap
+      if (arr[i] > arr[i+1]) [arr[i+1], arr[i]] = [arr[i], arr[i+1]];
     }
     upperBound--;
   }
-  return array;
+  return arr;
 }
+
+bubbleSort([37,45,29,8])
 
 bubbleSort(numbers);
 console.log(numbers);
