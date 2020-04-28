bc809d586b9a54591c0ee1f45928af00965717f3
Heng-Hao Pu <hpu@oswego.edu>
2020-04-28 18:09:41 -0400

    Implement selection sort with an example

--- sorting/selection-sort/app.js
+++ sorting/selection-sort/app.js
@@ -1,44 +1,23 @@
-const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
 
-// not the real selection sort 
-// but their general ideas are the same
-// like 7 to smoke + ranking 
-// defending champion system for 1st place, 2ng place, 3rd place, ...
-// Time: O(n * (3n))
-// Space: O(1)
-function selectionSort1(array) {
-  for (let i = 0; i < array.length - 1; i++) {
-    for (let j = i + 1; j < array.length; j++) {
-      if (array[j] < array[i]) {
-        // swap
-        const temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-      }
+/*    i=0... < length-1         initial champion = i
+      ↓   --> 
+  E: [2,3,4,5,1]
+        ↑   -->     
+        j=i+1... < length       IF arr[j] < arr[champ] THEN champ = j
+
+                              IF champ != i THEN swap
+ */
+
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let champ = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[champ]) champ = j;
     }
+    if (champ != i) [arr[i], arr[champ]] = [arr[champ], arr[i]];
   }
-  return array;
+  return arr;
 }
 
-// Time: O(n * (n + 3))
-// Space: O(1 + 2)
-function selectionSort2(array) {
-  for (let i = 0; i < array.length - 1; i++) {
-    let indexWithMinValue = i;
-    for (let j = i + 1; j < array.length; j++) {
-      if (array[j] < array[indexWithMinValue]) {
-        // record the smaller number
-        indexWithMinValue = j;
-      }
-    }
-    let temp = array[i];
-    array[i] = array[indexWithMinValue];
-    array[indexWithMinValue] = temp;
-  }
-  return array;
-}
-
-// selectionSort1(numbers);
-selectionSort2(numbers);
-console.log(numbers);
+console.log(selectionSort([2,3,4,5,1]))
