dcb3b85aee65b372ec42de14a522badde972de8f
Heng-Hao Pu <hpu@oswego.edu>
2020-05-04 14:25:18 -0400

    Implement mostDigits - use Math.max to replace if statement

--- sorting/radix-sort/radix-sort.js
+++ sorting/radix-sort/radix-sort.js
@@ -14,3 +14,14 @@ function digitCount(num) {
   return Math.floor(Math.log10(Math.abs(num))) + 1
 }
 
+// Given an array of numbers, return the number of digits in the largest number in the list
+// ([1234,56,7]) => 4, ([1,1,11111,1]) => 5 ([12,34,56,78]) => 2
+function mostDigits(nums) {
+  let maxDigits = -Infinity;
+  nums.forEach((num) => {
+    // let currentDigitCount = digitCount(num);
+    // if (currentDigitCount > maxDigits) maxDigits = currentDigitCount;
+    maxDigits = Math.max(maxDigits, digitCount(num));
+  })
+  return maxDigits;
+}
